968aae763895ec13a92e739174cef687ab33ada8
veonun <veonun@ttu.ee>
2018-04-16 03:03:09 +0300

    Open a text file and encrypt and decrypt with caesar cipher

---
+++ labone/src/caesarcipher.py
@@ -0,0 +1,41 @@
+
+key = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def encrypt_caesar(n, content):
+    result = ''
+    for l in content.lower():
+        try:
+            i = (key.index(l) + n) % 26
+            result += key[i]
+        except ValueError:
+            result += l
+    return result.lower()
+
+
+def decrypt_caesar(n, encryptedtxt):
+    result = ''
+    for l in encryptedtxt:
+        try:
+            i = (key.index(l) - n) % 26
+            result += key[i]
+        except ValueError:
+            result += l
+    return result
+
+
+f = open("one.txt", "r")
+if f.mode == 'r':
+    text = f.read().splitlines()
+    f.close()
+    for text in text:
+        print(text)
+
+offset = 1
+
+encrypted = encrypt_caesar(offset, text)
+print('Encrypted using caesar cipher :', encrypted)
+
+decrypted = decrypt_caesar(offset, encrypted)
+print('Decrypted using caser cipher:', text)
+
